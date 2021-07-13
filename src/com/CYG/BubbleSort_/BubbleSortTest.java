2cbb0ac1e8ba34192759e19f0e7e12b4bc039204
CindyYG <87381909+CindyYG@users.noreply.github.com>
2021-07-13 14:22:56 -0500

    1st version commit test

---
+++ src/com/CYG/BubbleSort_/BubbleSortTest.java
@@ -0,0 +1,19 @@
+package com.CYG.BubbleSort_;
+
+public class BubbleSortTest {
+    public static void main(String[] args) {
+        /*
+        Goal of Program:
+        1. Write my own code to implement bubble sort for an array.
+         */
+        /*
+        My Design:
+        1. initialize an int array
+        2. Find the largest number of the array, and save it as the last element in the array.
+        3. Repeat 2. to find the largest number in the rest (length-1) elements, and save it as the 2nd last element in the array.
+        4. Keep repeating until the array is sorted.
+         */
+        //Here is the code~
+        //Try git commit.
+    }
+}
